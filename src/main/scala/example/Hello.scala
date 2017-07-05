22ede6f3268c5f17b8acfcadd9be84cfc9feb0bd
Rafael Klaessen <rafaelklaessen.nl@gmail.com>
2017-07-05 19:10:11 +0200

    Added basic initing

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,6 +1,9 @@
 package example
 import net.rk02.firebasescala.Firebase
+import java.io.FileInputStream
 
 object Hello extends App {
+  val s = new FileInputStream("firebase-auth.json")
   println(Firebase.get)
+  Firebase.init(s, "fir-scala", "RejuhkemAHBnTzNDxc926y4v0K4Y1UsBebWl4p1S")
 }
--- src/main/scala/net/rk02/firebasescala/Firebase.scala
+++ src/main/scala/net/rk02/firebasescala/Firebase.scala
@@ -1,5 +1,24 @@
 package net.rk02.firebasescala
 
+import java.io.FileInputStream
+import com.google.firebase.{FirebaseApp, FirebaseOptions}
+import com.google.firebase.auth.FirebaseCredentials
+
 object Firebase {
+  private var dbSecret: String = ""
+
+  def init(serviceAccount: FileInputStream, projectId: String, dbSecret: String): Unit = {
+    if (FirebaseApp.getApps.isEmpty) {
+      val options = new FirebaseOptions.Builder()
+          .setCredential(FirebaseCredentials.fromCertificate(serviceAccount))
+          .setDatabaseUrl(s"https://$projectId.firebaseio.com")
+          .build
+
+      FirebaseApp.initializeApp(options)
+    }
+
+    Firebase.dbSecret = dbSecret
+  }
+
   def get = "test"
 }
